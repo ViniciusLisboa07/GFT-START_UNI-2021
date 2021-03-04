40df3521c4d27bd572106a2306df5fa32c5d32f5
Vinícius Lisboa <viniciuslisboa1001@gmail.com>
2021-03-04 16:16:58 -0300

    Exercicio 3

--- src/com/gft/exercicio3/Main.java
+++ src/com/gft/exercicio3/Main.java
@@ -7,7 +7,21 @@ public class Main {
     public static void main(String args[]){
         Scanner ler = new Scanner(System.in);
         System.out.printf("Informe o peso:\n");
-        
+        Double peso = ler.nextDouble();
+
+        System.out.printf("Informe o distancia:\n");
+        int distancia = ler.nextInt();
+
+        System.out.printf("Informe o valor:\n");
+        Double valor = ler.nextDouble();
+
+        Carga cargaVagao = new Carga(peso, valor);
+        Carga cargaCaminhao = new Carga(peso, valor);
+        Vagao vagao = new Vagao(cargaVagao);
+        Caminhao caminhao = new Caminhao(cargaCaminhao);
+
+        System.out.printls(vagao.calculaFrete(distancia));
+        System.out.printls(caminhao.calculaFrete(distancia));
 
     }
 }
